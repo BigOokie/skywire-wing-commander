fc16283e68ce37d6770b224183cbd0056fe59822
BigOokie <36015380+BigOokie@users.noreply.github.com>
2018-06-16 20:37:37 +1000

    Initial tests of file monitoring using fsnotify

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/telegram-bot-api.v4"
 )
 
 // The BotConfig struct is used to store run-time configuration
@@ -53,68 +53,108 @@ func allowedToRespondToChat(chatid int64) bool {
 	return true
 }
 
+// watchFile will watch the file specified by filename
+func watchFile(filename string) {
+	log.Debugf("Seting up file watch on file: %s", filename)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		log.Infof("Now watching file: %s", filename)
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Infof("File watcher(%s) handling event  [%s]", event.Name, event.Op)
+				} else {
+					log.Infof("File watcher(%s) ignorning event [%s]", event.Name, event.Op)
+				}
+			case err := <-watcher.Errors:
+				log.Errorln("File watcher error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+	<-done
+}
+
 func main() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.DebugLevel)
 	log.Infoln("Starting Telegram Notification Bot App.")
 	parseFlags()
 
-	// Create new telegram bot using the bot token passed on the cmd line
-	bot, err := tgbotapi.NewBotAPI(config.BotToken)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// Setup bot interface debugging
-	bot.Debug = config.BotDebug
-	log.Infof("Authorised on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates, err := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if update.Message == nil {
-			log.Warn("Ignoring empty message.")
-			continue
+	/*
+		// Create new telegram bot using the bot token passed on the cmd line
+		bot, err := tgbotapi.NewBotAPI(config.BotToken)
+		if err != nil {
+			log.Panic(err)
 		}
+	*/
 
-		log.Infof("Message recieved from ChatID: %v", update.Message.Chat.ID)
+	// Start watching the Skywire Monitors clients.json file
+	watchFile("test.json")
 
-		if allowedToRespondToChat(update.Message.Chat.ID) {
-			// Allowed to respond to this Chat ID
-			if update.Message.IsCommand() {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-				log.Debugf("Command: %s", update.Message.Command())
-				switch update.Message.Command() {
-				case "help":
-					msg.Text = "type /sayhi or /status or /chatid or /lock or /unlock."
-				case "sayhi":
-					msg.Text = "Hi :)"
-				case "status":
-					msg.Text = "I'm ok."
-				case "chatid":
-					msg.Text = fmt.Sprintf("ChatID: %v", update.Message.Chat.ID)
-				case "lock":
-					if config.ChatID == -1 {
-						config.ChatID = update.Message.Chat.ID
-						msg.Text = fmt.Sprintf("Locked to current ChatID [%v]", update.Message.Chat.ID)
-					} else {
-						msg.Text = "Already locked."
-					}
-				case "unlock":
-					if config.ChatID == -1 {
-						msg.Text = "Already unlocked."
-					} else {
-						config.ChatID = -1
-						msg.Text = "Unlocked."
-					}
-				default:
-					msg.Text = "Sorry. I don't know that command."
-				}
-				bot.Send(msg)
+	/*
+		// Setup bot interface debugging
+		bot.Debug = config.BotDebug
+		log.Infof("Authorised on account %s", bot.Self.UserName)
+
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+		updates, err := bot.GetUpdatesChan(u)
+		for update := range updates {
+			if update.Message == nil {
+				log.Warn("Ignoring empty message.")
+				continue
 			}
-		} else {
-			log.Warnf("Chat is locked to ID [%v]. Not allowed to respond to Chat ID [%v]", config.ChatID, update.Message.Chat.ID)
+
+			log.Infof("Message recieved from ChatID: %v", update.Message.Chat.ID)
+
+			if allowedToRespondToChat(update.Message.Chat.ID) {
+				// Allowed to respond to this Chat ID
+				if update.Message.IsCommand() {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+					log.Debugf("Command: %s", update.Message.Command())
+					switch update.Message.Command() {
+					case "help":
+						msg.Text = "type /sayhi or /status or /chatid or /lock or /unlock."
+					case "sayhi":
+						msg.Text = "Hi :)"
+					case "status":
+						msg.Text = "I'm ok."
+					case "chatid":
+						msg.Text = fmt.Sprintf("ChatID: %v", update.Message.Chat.ID)
+					case "lock":
+						if config.ChatID == -1 {
+							config.ChatID = update.Message.Chat.ID
+							msg.Text = fmt.Sprintf("Locked to current ChatID [%v]", update.Message.Chat.ID)
+						} else {
+							msg.Text = "Already locked."
+						}
+					case "unlock":
+						if config.ChatID == -1 {
+							msg.Text = "Already unlocked."
+						} else {
+							config.ChatID = -1
+							msg.Text = "Unlocked."
+						}
+					default:
+						msg.Text = "Sorry. I don't know that command."
+					}
+					bot.Send(msg)
+				}
+			} else {
+				log.Warnf("Chat is locked to ID [%v]. Not allowed to respond to Chat ID [%v]", config.ChatID, update.Message.Chat.ID)
+			}
 		}
-	}
+	*/
 }
