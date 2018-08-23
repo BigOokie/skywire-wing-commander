b9df2b89819f36fd7b256b10b8554014f28d1f63
BigOokie <36015380+BigOokie@users.noreply.github.com>
2018-08-23 23:08:07 +1000

    Ref #16 Updates (WIP):

--- src/scripts/wc-update.sh
+++ src/scripts/wc-update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Updating Wing Commander..."
 cd ${GOPATH}/src/github.com/BigOokie/skywire-wing-commander
@@ -9,28 +9,29 @@ go install -v  ./... 2>> /tmp/wingcommander_install_errors.log
 echo "Checking for Wing Commander Process..."
 cd ${GOPATH}/bin
 
-WCPID = pgrep wcbot
+WCPID=$(pgrep wcbot)
 
-if [$WCPID = ""]; then
+if [ "$WCPID" =  "" ]; then
     echo "Wing Commander does not appear to be running."
 else
-    echo "Wing Commander process identifed " $WCPID
+    echo "Wing Commander process identifed: $WCPID"
     echo "Terminating the process"
-    kill  $WCPID
+    kill  ${WCPID}
 fi
 
+echo ""
 echo "Check Wing Commander Version."
 ./wcbot -v
 
 echo "Starting Wing Commander (background)..."
-nohup ./wcbot /dev/null 2>&1 & echo $! > wcbot.pid&
+nohup ./wcbot /dev/null 2>&1 & echo $! > wcbot.pid& > /dev/null
 
 echo "Checking Wing Commander started..."
-WCPID = pgrep wcbot
+WCPID=$(pgrep wcbot)
 
-if [$WCPID = ""]; then
+if [ "$WCPID" = "" ]; then
     echo "Wing Commander does not appear to be running."
 else
-    echo "Wing Commander process identifed " $WCPID
+    echo "Wing Commander process identifed: $WCPID"
     echo "Wing Commander updated and restarted"
 fi
